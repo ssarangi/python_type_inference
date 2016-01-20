4bb698ac02e612d9a80539daea9bb328d71b751d
Satyajit Sarangi <satyajit.sarangi@gmail.com>
2016-01-19 23:35:00 -0800

    partially works

--- src/optimizer/structurizer.py
+++ src/optimizer/structurizer.py
@@ -37,10 +37,19 @@ class ControlFlowBlock:
         self.cmp_inst = None
         self.nested = None
 
+    def __str__(self):
+        s = "True: %s <--> False: %s" % (self.true_block, self.false_block)
+        return s
+
 class Nested:
-    def __init__(self, bb, next=None):
+    def __init__(self, bb, parent, next=None):
         self.bb = bb
         self.next = next
+        self.parent = parent
+
+    def __str__(self):
+        s = "Nested: %s" % self.bb
+        return s
 
 def find_culminating_block(start_bb):
     while not start_bb.is_empty() and start_bb.has_terminator():
@@ -56,48 +65,61 @@ class StructurizerAnalysisPass(FunctionPass, IRBaseVisitor):
         IRBaseVisitor.__init__(self)
         self.control_flow_stack = []
 
-    def get_current_visited_stack(self):
-        return self.visited_stack[-1]
-
-    def are_all_blocks_visited(self, func):
-        visited_stack = self.get_current_visited_stack()
-
-        for bb in func.basic_blocks:
-            if bb not in visited_stack:
-                return False
-
-        return True
-
     @verify(node=Function)
     def run_on_function(self, node):
         draw_header("Structurizer: %s" % node.name)
         func = node
 
         queue = Queue()
-        queue.put(Nested(func.entry_block))
-
+        queue.put(Nested(func.entry_block, None))
 
         visited = []
         while not queue.empty():
             nested_bb = queue.get()
             bb = nested_bb.bb
 
+            process = True
             if bb in visited:
                 # Then we have found a terminating condition.
-                pass
+                # Find the root off this control flow block
+                while not isinstance(nested_bb.parent, ControlFlowBlock):
+                    nested_bb = nested_bb.parent
 
-            terminator = bb.get_terminator()
-            if isinstance(terminator, ConditionalBranchInstruction):
-                # Start a visited stack
-                # Start with an If-else block
-                cfb = ControlFlowBlock()
-                cfb.true_block = Nested(terminator.bb_true)
-                cfb.false_block = Nested(terminator.bb_false)
+                cfb = nested_bb.parent
 
-                queue.put(Nested(terminator.bb_true))
-                queue.put(Nested(terminator.bb_false))
-            else:
-                assert isinstance(terminator, BranchInstruction)
-                # Find the branch where it terminates to
-                branch_to_bb = terminator.basic_block
-                nested_bb.next = Nested(branch_to_bb)
+                # Now lets look at which side of the cfb we are on.
+                if cfb.true_block == nested_bb:
+                    nested_bb = cfb.false_block
+                else:
+                    nested_bb = cfb.true_block
+
+                while nested_bb.bb != bb:
+                    nested_bb = nested_bb.next
+
+                # Now we found the Basic Block on the other end. This is the convergence point for the true-false block
+                # so erase all the children of this node.
+                queue.put(nested_bb.next)
+                nested_bb.next = None
+                process = False
+
+            visited.append(bb)
+
+            if process:
+                terminator = bb.get_terminator()
+                if isinstance(terminator, ConditionalBranchInstruction):
+                    # Start a visited stack
+                    # Start with an If-else block
+                    cfb = ControlFlowBlock()
+                    cfb.true_block = Nested(terminator.bb_true, cfb)
+                    cfb.false_block = Nested(terminator.bb_false, cfb)
+                    self.control_flow_stack.append(cfb)
+
+                    queue.put(cfb.true_block)
+                    queue.put(cfb.false_block)
+                elif isinstance(terminator, BranchInstruction):
+                    # Find the branch where it terminates to
+                    branch_to_bb = terminator.basic_block
+                    nested_bb.next = Nested(branch_to_bb, nested_bb)
+                    queue.put(nested_bb.next)
+                else:
+                    assert isinstance(terminator, ReturnInstruction)
